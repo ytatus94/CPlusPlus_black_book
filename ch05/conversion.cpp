af5733550eef62bb594307c62d520c2eac4a8bf0
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-11-04 16:23:12 -0700

    Update conversion.cpp

--- ch05/conversion.cpp
+++ ch05/conversion.cpp
@@ -1,8 +1,11 @@
 /*
 (type) 舊的型態轉換方法
 const_cast <type> (expression) 轉換掉常數型態
+  expression 是常數，用 const_cast <type> 把常數性質拿掉
 dynamic_cast <type> (expression) 在執行期識別由指標或參考指定的類別物件
+  用做即時型態檢查 ()
 reinterpret_cast <type> (expression) 對運算式的位元模式進行低階的重新解譯
+  用 reinterpret_cast 可以將記憶體位置直接給定給指標
 static_cast <type> (expression) 顯性型態轉換，不做即時型態檢查，關閉警告資訊
 */
 #include<iostream>
