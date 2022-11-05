e1f6f440c07b2feaef4556f8083b74897455d1bc
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-11-05 14:03:14 -0700

    Create vector.cpp

---
+++ ch16/vector.cpp
@@ -0,0 +1,27 @@
+/*
+和 ch15 的 vector.cpp 一模一樣
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+const int NUMBER_STUDENTS = 4;
+
+int main()
+{
+    vector<double> scores(NUMBER_STUDENTS);
+    double running_sum = 0;
+
+    scores[0] = 89.0;
+    scores[1] = 29.0;
+    scores[2] = 59.0;
+    scores[3] = 79.0;
+
+    for (unsigned int loop_index = 0; loop_index < scores.size(); loop_index++) {
+        running_sum += scores(loop_index);
+    }
+
+    cout << "The average student score is: "
+         << running_sum / scores.size() << endl;
+  
+    return 0;
+}
