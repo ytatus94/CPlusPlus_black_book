370ed5ab14691bd133b71ce981f8b0f46ee393be
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-11-06 00:15:43 -0700

    Create minimal.cpp

---
+++ ch17/minimal.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int NUMBER_ELEMENTS = 8;
+
+int main()
+{
+    int initial_values[NUMBER_ELEMENTS] = {1, 2, 3, 4, 5, 6, 6, 7};
+
+    vector<int> vector1(NUMBER_ELEMENTS);
+    vector1.assign(initial_values, initial_values + NUMBER_ELEMENTS);
+
+    vector<int>::iterator maximum_value = max_element(vector1.begin() vector1.end());
+    cout << "Maximum value: " << *maximum_value << endl;
+
+    vector<int>::iterator minimum_value = min_element(vector1.begin() vector1.end());
+    cout << "Minimum value: " << *minimum_value << endl;
+
+    return 0;
+}
