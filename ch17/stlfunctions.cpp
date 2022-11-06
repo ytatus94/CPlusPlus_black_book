7c4e8ab91913b7b771705f838502803b682c1955
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-11-06 12:59:04 -0800

    Update stlfunctions.cpp

--- ch17/stlfunctions.cpp
+++ ch17/stlfunctions.cpp
@@ -1,5 +1,7 @@
 /*
 和 ch15 的 stlfunctions.cpp 一模一樣的
+
+for_each(起點的 iterator, 終點的 iterator, 要執行的函數)
 */
 #include <iostream>
 #include <string>
@@ -13,7 +15,7 @@ class person
     int age;
 public:
     person(string n, int a) {name = n; age = a;}
-    friend bool compare_ages(const person & person1, const person & person2);
+    friend bool compare_ages(const person & person1, const person & person2); // 友誼函數可以直接存取類別的成員
     friend bool operator<(const person & person1, const person & person2);
     friend void display(person p);
 };
@@ -43,24 +45,24 @@ int main()
 {
     vector<person> friends;
 
-    friend.push_back(person("Ralph", 35));
-    friend.push_back(person("Tom", 36));
-    friend.push_back(person("Frank", 23));
-    friend.push_back(person("Sally", 24));
+    friends.push_back(person("Ralph", 35));
+    friends.push_back(person("Tom", 36));
+    friends.push_back(person("Frank", 23));
+    friends.push_back(person("Sally", 24));
 
-    sort(friends.begin(), friends.end());
+    sort(friends.begin(), friends.end()); // 預設用 operator<() 指定的方式排序
 
     cout << "Sorted by name..." << endl;
 
-    for_each(friends.begin(), friends.end(), display);
+    for_each(friends.begin(), friends.end(), display); // 把從起點到終點的所有元素都用 display 跑一次
 
-    sort(friends.begin(), friends.end(), compare_ages);
+    sort(friends.begin(), friends.end(), compare_ages); // 改成用 compare_ages() 指定的方式排序
 
     cout << "Sorted by age..." << endl;
 
     for_each(friends.begin(), friends.end(), display);
 
-    random_shuffle(friends.begin(), friends.end());
+    random_shuffle(friends.begin(), friends.end()); // 洗牌，打亂排序
 
     cout << "Randomly shuffled..." << endl;
 
