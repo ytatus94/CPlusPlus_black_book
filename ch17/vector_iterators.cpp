c8f57e08c8f13143c8390e2229ad18f8953b0965
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-11-06 21:41:37 -0800

    Update vector_iterators.cpp

--- ch17/vector_iterators.cpp
+++ ch17/vector_iterators.cpp
@@ -12,17 +12,49 @@ class person
     int age;
 public:
     void display() {cout << "Name: " << name << " age: " << age << endl;}
-    person(string n, int a) {name = n; age = a;}
+    person(string n, int a) {name = n, age = a;}
 };
 
 int main()
 {
     vector<person> friends;
-
+  
     friends.push_back(person("Ralph", 35));
     friends.push_back(person("Tom", 36));
     friends.push_back(person("Frank", 23));
     friends.push_back(person("Sally", 24));
+  
+    vector<person>::iterator p;
+    for (p = friends.begin(); p != friends.end(); p++)
+        p->display();
+  
+    // erase(start, end), 刪除 start ~ end 的元素，但是不包含 end
+    friends.erase(friends.begin() + 1, friends.begin() + 2);
+  
+    cout << "Got rid of one friends..." << endl;
+  
+    for (p = friends.begin(); p != friends.end(); p++)
+        p->display();
 
-    vector
+    // insert(p, t) 在 p 前面插入 t
+    vector<person> nancys_friends;
+  
+    nancys_friends.push_back(person("Tina", 35));
+    nancys_friends.push_back(person("Buddy", 36));
+
+    friends.insert(friends.begin(), nancys_friends.begin() + 1, nancys_friends.begin() + 2);
+  
+    cout << "Added two new friends..." << endl;
+
+    for (p = friends.begin(); p != friends.end(); p++)
+        p->display();
+
+    // swap()
+    nancys_friends.swap(friends);
+    cout << "Swapping your friends with Nancy's..." << endl;
+  
+    for (p = friends.begin(); p != friends.end(); p++)
+        p->display();
+  
+    return 0;
 }
